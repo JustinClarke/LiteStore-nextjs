9545fab9bc4d6471b103a153e920d08924fabbb9
Justin Clarke <justinclarke241@gmail.com>
2022-08-15 22:17:30 +0530

    linked item

--- pages/spaces/garuda/index.js
+++ pages/spaces/garuda/index.js
@@ -47,7 +47,7 @@ export default function index() {
                                         </div>
                                     </a>
                                 </Link>
-                                <Link href="/spaces/garuda/">
+                                <Link href="/spaces/garuda/vitro">
                                     <a className="p-2 md:p-4 w-full md:w-auto">
                                         <div className="border-2 hover:bg-vitro-green border-gray-200 py-2 md:py-4 rounded-lg transition ease-in-out delay-10 duration-200 hover:-translate-y-1 hover:scale-105">
                                             <p className="leading-relaxed">Vitro Naturals</p>
