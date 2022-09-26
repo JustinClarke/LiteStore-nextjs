fb00a44ac3180ef0c4f49d212d3f58c4a2421b4a
Justin Clarke <justinclarke241@gmail.com>
2022-09-26 16:29:10 +0530

    update

--- pages/layout/Header.js
+++ pages/layout/Header.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function header() {
 	return (
-		<header className="sticky top-0 z-50 flex flex-wrap pt-3 md:pt-0 sm:justify-start sm:flex-nowrap w-full bg-white shadow-md text-sm">
+		<header className="sticky top-0 z-50 w-full flex flex-wrap pt-3 md:pt-0 sm:justify-start sm:flex-nowrap bg-white shadow-md text-sm">
 			<nav className="md:px-40 lg:px-20 xl:px-40 mx-auto w-full sm:flex sm:items-center sm:justify-between" aria-label="Global">
 				<Link href="/">
 					<a className="px-3 flex-none text-4xl text-purple transition ease-in-out delay-10 duration-500 hover:scale-110">LiteStore</a>
