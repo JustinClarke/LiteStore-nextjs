9580b51a42da61c21aa55379db368f28d235cce0
Justin Clarke <justinclarke241@gmail.com>
2022-11-21 14:52:02 +0000

    update

---
+++ pages/spaces/garuda/skillmatics.js
@@ -0,0 +1,105 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import Header from '../../layout/Header'
+import Footer from '../../layout/Footer'
+
+import React from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, EffectFade, A11y, Autoplay } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+
+const skillmatics = '/spaces/garuda/skillmatics/skillmatics.webp'
+const skillmaticsInterior = '/spaces/garuda/skillmatics/skillmaticsInterior.webp'
+const skillmaticsInterior1 = '/spaces/skillmatics/zymrat/skillmaticsInterior1.webp'
+
+export default function mensxp() {
+    return (
+        <div>
+            <Head>
+                <title>Skillmatics | LiteStore</title>
+                <meta name="og:title" content="Wow | LiteStore" />
+                <meta name="twitter:title" content="Wow | LiteStore" />
+                <meta property="og:title" content="Wow | LiteStore" />
+
+            </Head>
+            <Header />
+            <div className="px-5 md:px-40 pt-5 md:pt-10 pb-10 bg-light-white">
+                <h1 data-aos="fade-up" className="text-6xl text-[#1D91CF] font-bold pb-2">Skillmatics <br className="invisible md:hidden" /><span className="text-black font-light"> Flexi-Store,</span><span className="text-black font-semibold"> Lulu Mall</span> </h1>
+                {/* <h1 data-aos="fade-up" className="text-2xl font-light py-3">November '21</h1> */}
+                <div data-aos="fade" className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
+                    <div className="flex col-span-2">
+                        <Swiper
+                            modules={[Navigation, Pagination, A11y, EffectFade, Autoplay]}
+                            effect="fade"
+                            slidesPerView={1}
+                            autoplay={true}
+                        // pagination={{ clickable: true }}
+                        >
+                            <SwiperSlide>
+                                <img
+                                    className="rounded-lg"
+                                    src={skillmatics}
+                                    alt="Skillmatics" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    className="rounded-lg"
+                                    src={skillmaticsInterior}
+                                    alt="Skillmatics" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    className="rounded-lg"
+                                    src={skillmaticsInterior1}
+                                    alt="Skillmatics" />
+                            </SwiperSlide>
+                        </Swiper>
+                    </div>
+                    <div className="flex col-span-1 justify-left text-justify">
+                        <div className="grid grid-cols-1 gap-6">
+                            <div data-aos="fade-up">
+                                <h1 className="text-4xl font-semibold pb-1">About</h1>
+                                <p className="font-light">
+                                The Skillmatics Flexi-Store at Garuda Mall offers innovative educational games to enhance a child's cognitive and motor skills.
+                                </p>
+                                <p className="font-light pt-2">
+                                Surrounded by brands such as Reliance Trends, Soch and Shoppers Stop - a Flexi-Store for kids forms the ideal tenancy mix in the mall.
+                                </p>
+                                {/* <div className="grid grid-cols-3 gap-6 pt-5 text-lg font-light">
+                                    <div>
+                                        <h1 className="text-4xl font-semibold pb-1">55%</h1>
+                                        <p className="leading-5">Daily Conversion Rate</p>
+                                    </div>
+                                    <div>
+                                        <h1 className="text-4xl font-semibold pb-1">1500+</h1>
+                                        <p className="leading-5">Pieces Sold</p>
+                                    </div>
+                                    <div>
+                                        <h1 className="text-4xl font-semibold pb-1">1st</h1>
+                                        <p className="leading-5">Exclusive offline store</p>
+                                    </div>
+                                </div> */}
+                                <div className="flex pt-8">
+                                    <a className="inline-flex text-white bg-purple border-0 py-3 px-6 focus:outline-none hover:bg-darker-purple active:bg-black rounded text-lg transition ease-in-out delay-10 duration-200 hover:-translate-y-1 hover:scale-110" href="https://skillmaticsworld.com" target="_blank" rel="noreferrer">
+                                        <span className="pr-3">Online Store</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
